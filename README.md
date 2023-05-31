72dafdb3b7bd41615327fd45c59bab52d257207e
AlcorBBX <lev.matisov55@gmail.com>
2023-06-01 01:23:08 +0500

    fix main.rs

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # YewApp
 
 `trunk serve` <br/>
-link: `localhost:8080`
-
-`npx create-yew-app my-app`
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,19 @@
 use yew::prelude::*;
 
 struct Model {
-    value: i64
+    value: i64,
 }
 
 #[function_component(App)]
 fn app() -> Html {
-    let state = use_state(|| Model {
-        value: 0
-    });
+    let state = use_state(|| Model { value: 0 });
 
     let onclick = {
         let state = state.clone();
-    
 
         Callback::from(move |_| {
             state.set(Model {
-                value: state.value + 1
+                value: state.value + 1,
             })
         })
     };
@@ -30,5 +27,5 @@ fn app() -> Html {
 }
 
 fn main() {
-    yew::start_app::<App>();
+    yew::Renderer::<App>::new().render();
 }
